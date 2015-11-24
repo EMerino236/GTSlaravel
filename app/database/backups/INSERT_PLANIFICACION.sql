3639649828613b38725f01a875b18f6c1d8f4805
gustavocoronado <gustavo.coronado@pucp.pe>
2015-11-24 15:05:15 -0500

    Commit

---
+++ app/database/backups/INSERT_PLANIFICACION.sql
@@ -0,0 +1,8 @@
+INSERT INTO tipo_reporte_PAAC (idtipo_reporte_PAAC,nombre) VALUES ('1','R-PAAC');
+INSERT INTO tipo_reporte_PAAC (idtipo_reporte_PAAC,nombre) VALUES ('2','R-PAAC Complementario');
+INSERT INTO tipo_reporte_CN (idtipo_reporte_CN,nombre) VALUES ('1','R-CN Anual');
+INSERT INTO tipo_reporte_CN (idtipo_reporte_CN,nombre) VALUES ('2','R-CN Inmediato');
+INSERT INTO tipo_reporte_CN (idtipo_reporte_CN,nombre) VALUES ('3','R-CN Mediano y Largo Plazo');
+INSERT INTO tipo_reporte_ETES (idtipo_reporte_ETES,nombre) VALUES ('1','R-ETES Clínico');
+INSERT INTO tipo_reporte_ETES (idtipo_reporte_ETES,nombre) VALUES ('2','R-ETES Tecnológico');
+INSERT INTO tipo_reporte_ETES (idtipo_reporte_ETES,nombre) VALUES ('3','R-ETES Económico');
