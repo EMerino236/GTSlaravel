267f63b7eb8decb0f5927bc3e87de4966da98a39
TAMQuiroz <tamquiroz@gmail.com>
2015-12-14 21:27:21 -0500

    migraciones

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Thujohn\Pdf\PdfServiceProvider',
+		'Way\Generators\GeneratorsServiceProvider',
+		'Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider',
 	),
 
 	/*
