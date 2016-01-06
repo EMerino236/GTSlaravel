5c562b4e997a4b1d3ac85daccbed8b07502e38a2
TAMQuiroz <tamquiroz@gmail.com>
2016-01-06 10:59:32 -0500

    commit

--- app/database/migrations/2015_12_28_140302_add_fk_id_guia_to_programacion_guia_ts_table.php
+++ app/database/migrations/2015_12_28_140302_add_fk_id_guia_to_programacion_guia_ts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFkToProgramacionGuiaTsTable extends Migration {
+class AddFkIdGuiaToProgramacionGuiaTsTable extends Migration {
 
 	/**
 	 * Run the migrations.
--- app/database/migrations/2015_12_30_205714_add_fk_id_guia_to_programacion_guia_gpc_table.php
+++ app/database/migrations/2015_12_30_205714_add_fk_id_guia_to_programacion_guia_gpc_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFkToProgramacionGuiaGpcTable extends Migration {
+class AddFkIdGuiaToProgramacionGuiaGpcTable extends Migration {
 
 	/**
 	 * Run the migrations.
