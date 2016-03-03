7fd4748d5cfcd1f28180679a04149c33f3486306
Kei T <takayama.k@pucp.edu.pe>
2016-03-03 12:02:41 -0500

    RRHH
    -Se creó el Template
    -Se creó el sidebar

---
+++ app/views/templates/recursosHumanosTemplate.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+    <meta name="robots" content="noindex, follow">
+    <title>Recursos Humanos</title>
+    <link rel="shortcut icon" href="{{ asset('img')}}/logo_gts.png">
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Datepicker CSS-->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-datetimepicker.min.css') }}">
+    <!-- MetisMenu CSS -->
+    <link href="{{ asset('bower_components/metisMenu/dist/metisMenu.min.css') }}" rel="stylesheet">
+    <!-- Timeline CSS -->
+    <link href="{{ asset('dist/css/timeline.css') }}" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{ asset('dist/css/sb-admin-2.css') }}" rel="stylesheet">
+    <!-- Morris Charts CSS -->
+    <link href="{{ asset('bower_components/morrisjs/morris.css') }}" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fileinput.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/tab-menus.css') }}" rel="stylesheet">
+    <!--Bootstrap-Dialog CSS-->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-dialog.min.css') }}">
+    <script type="text/javascript">
+        var inside_url = "{{$inside_url}}";
+    </script>
+
+    <!-- jQuery -->
+    <script src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- Moment JavaScript -->
+    <script type="text/javascript" src="{{ asset('js/moment.min.js') }}"></script>
+    <!-- Bootstrap Datepicker JavaScript -->
+    <script type="text/javascript" src="{{ asset('js/bootstrap-datetimepicker.min.js') }}"></script>
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="{{ asset('bower_components/metisMenu/dist/metisMenu.min.js') }}"></script>
+    <!-- Morris Charts JavaScript -->
+    <script src="{{ asset('bower_components/raphael/raphael-min.js') }}"></script>
+    <script src="{{ asset('bower_components/morrisjs/morris.min.js') }}"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="{{ asset('dist/js/sb-admin-2.js') }}"></script>    
+    <script src="{{ asset('js/fileinput.min.js') }}"></script>
+    <script src="{{ asset('js/fileinput_locale_es.min.js') }}"></script>
+    <script src="{{ asset('js/language_file_upload_plugin.min.js') }}"></script>
+    <!--Bootstrap-Dialog Javascritp-->
+    <script src="{{asset('js/bootstrap-dialog.min.js') }}"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>-->
+    
+</head>
+
+<body>
+	<div id="wrapper">
+		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+		@include('layouts.header', array('user'=>$user))
+		@include('layouts.sidebarRecursosHumanos', array('user'=>$user))
+        </nav>
+        <div id="page-wrapper">
+        	@yield('content')
+        </div>
+	</div>
+</body>
+</html>
